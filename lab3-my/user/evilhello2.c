11b856e90e00cec7c880e8fe134cb2e9b358b946
luweijia <luweijia1013@sjtu.edu.cn>
2017-05-28 23:46:24 +0800

    first test for evilhello2

--- lab3-my/user/evilhello2.c
+++ lab3-my/user/evilhello2.c
@@ -33,6 +33,16 @@ sgdt(struct Pseudodesc* gdtd)
 	__asm __volatile("sgdt %0" :  "=m" (*gdtd));
 }
 
+//my wrapper fun of evil(but how to pass arguments like gdt,ori and func_ptr evil)
+void my_evil(){
+	evil();
+	gdt[GD_UD>>3] = ori;
+	asm volatile("pop %ebp");
+	asm volatile("lret");
+}
+
+struct Segdesc *gdt;
+struct Segdesc ori;
 // Invoke a given function pointer with ring0 privilege, then return to ring3
 void ring0_call(void (*fun_ptr)(void)) {
     // Here's some hints on how to achieve this.
@@ -49,6 +59,22 @@ void ring0_call(void (*fun_ptr)(void)) {
     //        file if necessary.
 
     // Lab3 : Your Code Here
+    struct Pseudodesc *gdtd;
+    //struct Segdesc gdt[4096]={0};
+    char respage[PGSIZE];//avoid conflict between user original page and map page.
+    char gdtmap[PGSIZE];
+    sgdt(gdtd);
+    //uint32_t i,pi = 0;
+    //for(i = ROUNDDOWN(gdtd->pd_base, PGSIZE); i < ROUNDUP(gdtd->pd_base + gdtd->pd_limit, PGSIZE); i += PGSIZE, pi++){
+    	sys_map_kernel_page((void*) gdtd->pd_base, gdtmap)；
+    //}
+    uint32_t gdtpginit = ROUNDDOWN(gdtmap, PGSIZE);
+    uint32_t gdtpgoff = gdtd->pd_base - ROUNDDOWN(gdtd->pd_base);
+    uint32_t gdtaddr = gdtpginit + gdtpginit;
+    gdt = (struct Segdesc *)gdtaddr;
+    ori = gdt[GD_UD>>3];
+    SETCALLGATE(gdt[GD_UD], GD_KT, my_evil, 3);
+	asm volatile("lcall $0x20, $0");
 }
 
 void
